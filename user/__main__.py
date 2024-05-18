a15a68b29811bf232a7752a5b30d736c8b5a25ca
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-05-18 19:40:31 +0530

    Create __main__.py

---
+++ user/__main__.py
@@ -0,0 +1,14 @@
+from user import *
+from pyrogram import Client, filters
+import os
+import io
+import pyrogram
+from subprocess import getoutput as run
+from Restart import restart_program
+
+
+PWD = f"{os.getcwd()}/"
+
+
+if __name__ == "__main__":
+    piro.run()
