5ce77058f712b922f96d2ff850340091546106d4
Tanjirotg <156563417+Tanjirotg@users.noreply.github.com>
2024-05-18 19:46:53 +0530

    Update __main__.py

--- user/__main__.py
+++ user/__main__.py
@@ -1,14 +1,4 @@
 from user import *
-from pyrogram import Client, filters
-import os
-import io
-import pyrogram
-from subprocess import getoutput as run
-from Restart import restart_program
-
-
-PWD = f"{os.getcwd()}/"
-
 
 if __name__ == "__main__":
     piro.run()
